a041c3c8d29f677d8d3f4903a29aa5d91c62dbe8
Isidro Morla <euris110811@gmail.com>
2020-04-08 13:29:55 -0400

    First change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 function ViewModel()  {
-    
+    this.name = ko.observable('Tabby')
+    this.clicks = ko.observable(0)
+    this.src = 'img/cat.jpg'
+    this.incrementerCount = function() {
+        this.clicks(this.clicks() + 1)
+    }
 }
 
 ko.applyBindings(new ViewModel())
