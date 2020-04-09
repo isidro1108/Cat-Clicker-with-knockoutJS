f289cf3ea503d9ad1381e0ea17984963e6e1627b
Isidro Morla <euris110811@gmail.com>
2020-04-08 21:03:16 -0400

    The cat haves a nicknames list and new levels

--- js/app.js
+++ js/app.js
@@ -2,14 +2,18 @@ function ViewModel()  {
     this.name = ko.observable('Tabby')
     this.clicks = ko.observable(0)
     this.imgSrc = 'img/cat.jpg'
+    this.nickNames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha'])
 
     this.incrementerCount = function() {
         this.clicks(this.clicks() + 1)
     }
     this.returnLevel = ko.computed(function() {
         return this.clicks() < 10 ? ko.observable('Newborn'):
-                this.clicks() < 100 ? ko.observable('Infant'):
-                ko.observable('Teen')
+                this.clicks() < 50 ? ko.observable('Infant'):
+                this.clicks() < 100 ? ko.observable('Child'):
+                this.clicks() < 200 ? ko.observable('Teen'):
+                this.clicks() < 500 ? ko.observable('Adult'):
+                ko.observable('Ninja')
     }, this)
 }
 
