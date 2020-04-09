d5dee2ada9b5c6a0dc3bad5904140263d0174a4a
Isidro Morla <euris110811@gmail.com>
2020-04-08 21:18:38 -0400

    Separated out the model

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-function ViewModel()  {
+var Cat = function() {
     this.name = ko.observable('Tabby')
     this.clicks = ko.observable(0)
     this.imgSrc = 'img/cat.jpg'
     this.nickNames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha'])
 
-    this.incrementerCount = function() {
-        this.clicks(this.clicks() + 1)
-    }
     this.returnLevel = ko.computed(function() {
         return this.clicks() < 10 ? ko.observable('Newborn'):
                 this.clicks() < 50 ? ko.observable('Infant'):
@@ -15,6 +12,14 @@ function ViewModel()  {
                 this.clicks() < 500 ? ko.observable('Adult'):
                 ko.observable('Ninja')
     }, this)
+}
+
+function ViewModel()  {
+    this.currentCat = ko.observable(new Cat())
+
+    this.incrementerCount = function() {
+        this.currentCat().clicks(this.currentCat().clicks() + 1)
+    }
 }
 
 ko.applyBindings(new ViewModel())
