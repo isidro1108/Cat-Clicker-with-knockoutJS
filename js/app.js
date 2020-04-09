20595bdb16ea58b415f19bc7a5a3ab8e3ed26416
Isidro Morla <euris110811@gmail.com>
2020-04-08 20:29:45 -0400

    The levels has been added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 function ViewModel()  {
     this.name = ko.observable('Tabby')
     this.clicks = ko.observable(0)
-    this.src = 'img/cat.jpg'
+    this.imgSrc = 'img/cat.jpg'
+
     this.incrementerCount = function() {
         this.clicks(this.clicks() + 1)
     }
+    this.returnLevel = ko.computed(function() {
+        return this.clicks() < 10 ? ko.observable('Newborn'):
+                this.clicks() < 100 ? ko.observable('Infant'):
+                ko.observable('Teen')
+    }, this)
 }
 
 ko.applyBindings(new ViewModel())
